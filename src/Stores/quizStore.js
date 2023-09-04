43173cd71b3bd3885cccc996ba768ada7e95b1b5
rahmani djamel <hxhdono11@gmail.com>
2023-09-05 01:03:01 +0200

    new fixes

---
+++ src/Stores/quizStore.js
@@ -0,0 +1,41 @@
+import { defineStore } from "pinia";
+
+
+export let useQuizStore = defineStore('quiz',{
+    state(){
+        return {
+            questions: [
+                {
+                    question: "What is the capital of France?",
+                    options: [
+                        "Paris",
+                        "London",
+                        "Berlin",
+                        "Madrid"
+                    ],
+                    correctAnswer: "Paris"
+                },
+                {
+                    question: "Which planet is known as the Red Planet?",
+                    options: [
+                        "Earth",
+                        "Mars",
+                        "Venus",
+                        "Jupiter"
+                    ],
+                    correctAnswer: "Mars"
+                },
+                {
+                    question: "Who wrote the play 'Romeo and Juliet'?",
+                    options: [
+                        "Charles Dickens",
+                        "William Shakespeare",
+                        "Jane Austen",
+                        "F. Scott Fitzgerald"
+                    ],
+                    correctAnswer: "William Shakespeare"
+                }
+            ]
+        };
+    }
+})
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,28 +4,57 @@
  
   </div>
   <div>
-            <p class="text-2xl font-bold">Rolex is a company that specializes in what type of product?</p>
+            <p class="text-2xl font-bold">{{quiz.questions[0].question}}</p>
 
-                        <label
-
-              class="block mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg hover:bg-gray-100 cursor-pointer"
-            >
-              <input
-                
-                type="radio"
-                class="hidden"
-              />
-              hello
-            </label>
+    <label
+      v-for="(option, index) in quiz.questions[0].options"
+      :key="index"
+      :for="index"
+      class="block mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg"
+      :class="{
+        'hover:bg-gray-100 cursor-pointer': selected == '',
+        'bg-green-500 text-white': (selected == correctAnswer && index == Qindex) || selected != '' && correctAnswer == option,
+        'bg-red-500 text-white': selected != correctAnswer && index == Qindex,
+        'cursor-not-allowed': selected !== '' // Add this class to change cursor style
+      }"
+    >
+      <input
+        :id="index"
+        type="radio"
+        class="hidden"
+        :value="index"
+        :disabled="selected !== ''"
+        @change="handleRadioChange(option, index)"
+      />
+      {{ option }}
+    </label>
 
   </div>
 </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+   import { useQuizStore } from '@/Stores/quizStore.js'
+   import { ref } from 'vue'
+
+
+
+
+
+   let quiz = useQuizStore();
+
+   
+   let selected= ref("");
+   let Qindex = ref(null)
+
+
+   let correctAnswer = quiz.questions[0].correctAnswer
+
+   const handleRadioChange = (value,index) => {
+    selected.value = value;
+    Qindex.value = index
+   swal("Good job!", `You clicked the button ${selected} and correct answer is ${correctAnswer}`, "success")
+   }
 </script>
 
 <style lang="scss" scoped>
